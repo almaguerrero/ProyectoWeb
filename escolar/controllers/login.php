29395ea85fdae3ce8c7efb8ffa71211400b3145b
RolandoCM <castillor493@gmail.com>
2017-08-31 00:34:28 -0500

    Add files via upload

--- escolar/controllers/login.php
+++ escolar/controllers/login.php
@@ -24,9 +24,38 @@ $query = sprintf("SELECT User FROM Usuarios WHERE User= '".htmlentities($_POST["
 
 $myusuario = mysqli_query($database, $query);
 $nmyusuario= mysqli_num_rows($myusuario);
-
-if($nmyusuario!=0)
+if($nmyusuario==0)
 {
+    $query = sprintf("SELECT user FROM Alumnos WHERE user= '".htmlentities($_POST["UserName"]) ."'");
+    $myAlumno = mysqli_query($database, $query);
+    $nmyAlumno= mysqli_num_rows($myAlumno);
+    if($nmyAlumno!=0)
+    {
+        $sql =sprintf("SELECT user FROM  Alumnos WHERE user = '".htmlentities($_POST["UserName"])."' AND password = '".htmlentities($_POST["Password"])."'");
+        $myclave = mysqli_query($database, $sql);
+        $nmyclave = mysqli_num_rows($myclave);
+        
+        //Si usuario y clave son correctos
+        if($nmyclave != 0)
+        {
+            session_start();
+            //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario
+            $_SESSION["autentica"] = "SIP";
+            $fila = mysqli_fetch_array($myclave);
+            $_SESSION["usuarioactual"] = $fila['Nombre']; //nombre del usuario logueado.
+            header("Location: ../views/inicio.html");
+        }
+        else
+        {
+            print "La contraseña o usuario son incorrectas";
+        }
+    }
+    else
+    {
+        print ("El usuario no existe");
+    }
+}
+else {
     $sql =sprintf("SELECT User FROM  Usuarios WHERE User = '".htmlentities($_POST["UserName"])."' AND password = '".htmlentities($_POST["Password"])."'");
     $myclave = mysqli_query($database, $sql);
     $nmyclave = mysqli_num_rows($myclave);
@@ -38,26 +67,20 @@ if($nmyusuario!=0)
         //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario
         $_SESSION["autentica"] = "SIP";
         $fila = mysqli_fetch_array($myclave);
-        $_SESSION["usuarioactual"] = $fila['Nombre']; //nombre del usuario logueado.
-        $query_type_user = sprintf("SELECT tipo_usuario_idtipo_usuario FROM Usuarios WHERE User='" .htmlentities($_POST["UserName"]) ."'");
-        $type_user = mysqli_query($database, $query_type_user);
+        $_SESSION["usuarioactual"] = $fila['nombre']; //nombre del usuario logueado.
+        //$query_type_user = sprintf("SELECT tipo_usuario_idtipo_usuario FROM Usuarios WHERE User='" .htmlentities($_POST["UserName"]) ."'");
+        //$type_user = mysqli_query($database, $query_type_user);
         
-        $fila = mysqli_fetch_assoc($type_user);
+        //$fila = mysqli_fetch_assoc($type_user);
         
-        if($fila["tipo_usuario_idtipo_usuario"]==1){
-            header("Location: ../views/menu.html");
-        }
+        //if($fila["tipo_usuario_idtipo_usuario"]==1){
+            header("Location: ../views/inicio.html");
+        //}
     }
     
-    else
-    {
-        print "La contraseña o usuario son incorrectas";
-    }
 }
-else
-{
-    print ("El usuario no existe");
-}
+
+
 $db->close();
 
 
