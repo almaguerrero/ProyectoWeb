120d109b468cf3a21f2f8917a9d7c9da6d1da3e8
RolandoCM <castillor493@gmail.com>
2017-08-31 00:48:36 -0500

    Add files via upload
    scrip sql con insert para tablas de base de datos

---
+++ insertSQLweb.sql
@@ -0,0 +1,32 @@
+/*isert para tipo de usuario*/
+insert into tipo_usuario(idtipo_usuario, Descripcion)
+values (2, 'Cordinador');
+
+/* INSERT para Usuarios*/
+insert into Usuarios (User, password, tipo_usuario_idtipo_usuario, correo, estatus)
+values ('Rodrigo','password', 3, 'rodrigo@school.com', 1);
+
+/*INSERT para Alumnos*/
+insert into Alumnos(nombre, apellido, correo, user, password, estado)
+values ('Rafael', 'Oleo', 'oleo_r@gmail.com', 'rafael', 'password', 1);
+
+/*INSERT para Materias*/
+insert into Materias (idMaterias, nombre_materia)
+values (1,'Matematicas');
+
+
+/*INSERT para grupos*/
+insert into Grupos (Descripcion, capacidad)
+values ('A1',30);
+
+/*INSERT para Grupos_has_Alumnos*/
+insert into Grupos_has_Alumnos (Grupos_idGrupos, Alumnos_idAlumnos)
+values (1,1); 
+
+/*INSERT para Grupos_has_Materias*/
+insert into Grupos_has_Materias (Grupos_idGrupos, Materias_idMaterias)
+values (1,1);
+
+/*INSERT para Materias_has_Alumnos*/
+insert into Usuarios_has_Materias (Usuarios_idUsuarios, Materias_idMaterias)
+values (2,1);
