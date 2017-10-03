633e768f8d07716a8ec70afd033b5a7f878812a0
pedrOchoa <31259491+pedrOchoa@users.noreply.github.com>
2017-10-03 16:18:07 -0500

    Update Calificaciones.php

--- escolar/views/Calificaciones.php
+++ escolar/views/Calificaciones.php
@@ -4,37 +4,44 @@ require '../controllers/misCalificacionesAlumno.php';
 <html>
 <head>
 	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="css/calificaciones.css">
 	<title>Aministración</title>
-	    <!-- boostrap -->
-    <link href="../libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 	
 </head>
 <body>
-	<div id="contenido" >
-	<div class="panel panel-primary">
-	<div class="panel-heading">
-		<h3 class="panel-title">Lista de Usuarios</h3>
-	</div>
-	
-	<div class="panel-body">
-		<table class="table table-striped">
-			<thead>
-				<th>ID</th>
-				<th>GRUPO</th>
-				<th>MATERIA</th>
-				<th>CALIFICACION</th>
-			</thead>
-			<tbody>
-			<?php
-				MisCalificaciones::mostrarCalificaciones();
-			?>
-            </tbody>
-         </table>
+
+      <div class="w3-container w3-red ">
+       <h1 class="w3-center w3-animate-left">Lista de Calificaciones</h1>
+      </div>
+
+		<div class="w3-container">
+
+			<div class="w3-border">
+				 <div class="w3-container w3-margin">
+                  	  <table class="w3-table-all">
+   						 <thead>
+      						<tr class="w3-teal">
+       						 <th>ID</th>
+				             <th>GRUPO</th>
+				             <th>MATERIA</th>
+				             <th>CALIFICACION</th>
+     						</tr>
+    					</thead>
+                      		 <tbody>
+								<?php
+										MisCalificaciones::mostrarCalificaciones();
+								?>
+                      		 </tbody>
+        		      </table>
        </div>
-        <form action="../controllers/misCalificacionesReporte.php" target="_Blank" method="POST">
+       					 <form action="../controllers/misCalificacionesReporte.php" target="_Blank" method="POST">
             <button type="submit" class="waves-effect waves-teal btn-flat">Imprimir &nbsp; <i class="zmdi zmdi-mail-send"></i></button>
-        </form>
-    </div>
-  </div>
+      					  </form>
+                </div>
+			</div>
+				
 </body>
 </html>
